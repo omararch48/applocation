8ce006daef21a234660d8f783191684a050a0417
Omar Daniel Esquivel Sanchez <omardanielesquivel@gmail.com>
2024-02-23 06:08:36 -0600

    users fix 3

--- location/settings/prod.py
+++ location/settings/prod.py
@@ -9,8 +9,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_CREDENTIALS = False
 CSRF_TRUSTED_ORIGINS = [
-    'http://*',
-    'https://*',
+    'https://apilocation.omardanielesquivel.com',
 ]
 CORS_ALLOWED_ORIGINS = [
     'http://*',
